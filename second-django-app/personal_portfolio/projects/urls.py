b805ff3d73899d12cecdefb1cc988c0830338176
ameerhkhan <ameer_h.khan@rocketmail.com>
2020-09-08 21:07:08 +0500

    Comeback

--- second-django-app/personal_portfolio/projects/urls.py
+++ second-django-app/personal_portfolio/projects/urls.py
@@ -3,6 +3,7 @@
 from django.urls import path
 from . import views
 
+# root folder indicated via ""
 urlpatterns = [
     path("", views.project_index, name="project_index"),            # the root url of our projects app is hooked up to project_index view.
     path("<int:pk>/", views.project_detail, name="project_detail"), # since we require a pk hence that is also passed in this path so as to diplay the correct project.
